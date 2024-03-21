05a236c53ce859e8d5c84964569654d27ec834ab
luizatauchert <162368656+luizatauchert@users.noreply.github.com>
2024-03-21 17:13:12 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,10 @@
+function calculaAreaRetangulo(altura, largura){
+let area = altura * largura
+console.log(`a area é ${area}.`)
+}
+calculaAreaRetangulo(2,5)
+calculaAreaRetangulo(3,7)
+calculeAreaRetangulo(4,7)
+
+
+
---
+++ olamundo.js
@@ -0,0 +1,62 @@
+//exercicio1
+function ola(){
+    console.log("ola mundo")
+}
+ola()
+
+
+
+//exercico2
+function imprimeNota1 (nota1, nota2, nota3){
+    const media = (nota1 + nota2 + nota3)/3
+    console.log(media.toFixed(2))
+}
+
+imprimeNota1(10,20,30)
+
+
+//exercicio3
+function verificaNumero(numero){
+    if (numero % 2 ===0) {
+    console.log(`o numero ${numero} é par`)
+    } else {
+        console.log (`o numero ${numero} é impar`)
+
+}}
+verificaNumero (67)
+
+
+//exercicio4
+function imprimaNota(nota1,nota2,nota3){
+    const media = (nota1 + nota2 + nota3)/3
+    console.log(media.toFixed(2))
+}
+imprimaNota(20,10,30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
