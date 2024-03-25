c91e91f11b6477476cfb26688c7e2200defe4fc8
luizatauchert <162368656+luizatauchert@users.noreply.github.com>
2024-03-25 17:12:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,123 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+print('Hello World');
+
+var numero1 = 5;
+var numero2 = 3;
+var resultado ;
+ressultado = numero1==numero2;
+print(numero1, "=", numero2,"?",resultado);
+resultado = numero1 != numero2;
+print(numero1, "!=", numero2,"?",resultado);
+resultado = numero1 > numero2;
+print(numero1, ">", numero2,"?",resultado);
+resultado = numero1 < numero2;
+print(numero1, "<", numero2,"?",resultado);
+resultado = numero1 <= numero2;
+print(numero1, "<=", numero2,"?",resultado);
+resultado = numero1 >= numero2;
+print(numero1, ">=", numero2,"?",resultado);
+var a1 = true && true;
+print(a1);
+var a2 = true && false;
+print(a2);
+var a3 = false && true;
+print(a3);
+var a4 = false && 3 == 4;
+print(a4);
+var a5 = "gato" && "cão";
+print(a5);
+var a6 = false && "gato";
+print(a6);
+var a7 = "gato" && false;
+print(a7);
+
+
+var a1 = true || true;
+print(a1);
+var a2 = true || false;
+print(a2);
+var a3 = false || true;
+print(a3);
+var a4 = false || 3 == 4;
+print(a4);
+var a5 = "gato" || "cão";
+print(a5);
+var a6 = false || "gato";
+print(a6);
+var a7 = "gato" || false;
+print(a7);
+
+
+
+
+var palavra1 = 'lui'
+var palavra2 = 'za'
+var resultado = palavra1 + palavra2;
+print(resultado)
+
+
+
+
+
+var idade = 18;
+var status = idade >= 18? 'adulto':'menor de idade';
+print(status);
+
+
+
+var a = 9
+var b = 10
+var c = 9
+if((a>10 && a<20) || (b = 5)){
+    print('true')
+} else {
+    print('false')
+}
+
+if ((a>10) || (b == 5)){
+    print('true')
+}else{
+    
+print('false')
+}
+if (a>10 && a < 20){
+    print('true')
+}else{
+   print('false')
+}
+if ((a> 10)||(b=5) && (c===10)){
+    print('true')
+}else{
+    print ('false')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
